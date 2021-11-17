08ccf287fcfc3cfa14479198d1e9f69fda9371f4
Akash.Aggarwal <aaggarwal@c02dq8v4md6r.corp.intusurg.com>
2021-11-17 11:57:18 -0800

    added speech to text

---
+++ static/script.js
@@ -0,0 +1,74 @@
+try {
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+  $('.no-browser-support').show();
+  $('.app').hide();
+}
+
+
+var noteTextarea = $('#note-textarea');
+var instructions = $('#recording-instructions');
+var noteContent = '';
+
+/*-----------------------------
+      Voice Recognition
+------------------------------*/
+
+recognition.continuous = true;
+
+// This block is called every time the Speech APi captures a line.
+recognition.onresult = function(event) {
+
+  var current = event.resultIndex;
+
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript;
+  console.log("Transript");
+  console.log(transcript);
+
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    noteContent = transcript;
+    noteTextarea.val(noteContent);
+  }
+};
+
+recognition.onstart = function() {
+  instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');
+  };
+}
+
+/*-----------------------------
+      App buttons and input
+------------------------------*/
+
+$('#start-record-btn').on('click', function(e) {
+  if (noteContent.length) {
+    noteContent += ' ';
+  }
+  recognition.start();
+});
+
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+  instructions.text('Voice recognition paused.');
+});
+
+// Sync the text inside the text area with the noteContent variable.
+noteTextarea.on('input', function() {
+  noteContent = $(this).val();
+})
